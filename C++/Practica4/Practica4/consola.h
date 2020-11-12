5875cbb88e41383548f502d8f0065e2d06e983af
Peter <petcutmat@alum.us.es>
2020-11-12 19:17:09 +0100

    Mates 2 progreso

--- C++/Practica4/Practica4/consola.h
+++
@@ -1,6 +0,0 @@
-#include <conio.h>
-#include <windows.h>
-
-void gotoxy			(int x, int y);
-void hidecursor	(void);
-void clear			();
--- Math/exercise1handout/exercise2handout/exercise2.cpp
+++
@@ -1,5 +0,0 @@
-#include "exercise2.h"
-
-bool Exercise2::isInputEnabled  = true;
-double Exercise2::mousePosX, Exercise2::mousePosY;
-double Exercise2::prevMousePosX, Exercise2::prevMousePosY;
